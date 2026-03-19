1ddcc4f27ffec247ee909d407aaaf2fee68e15c1
LYWW0120 <88764790+LYWW0120@users.noreply.github.com>
2026-03-20 00:02:12 +0900

    prologue update

--- Assets/1.Scirpts/yw/PrologueVideoController.cs
+++ Assets/1.Scirpts/yw/PrologueVideoController.cs
@@ -7,15 +7,26 @@ public class PrologueVideoController : MonoBehaviour
     [SerializeField] private VideoPlayer videoPlayer;
     [SerializeField] private Transform mainMapSpawnPoint;
     [SerializeField] private LocomotionEventsConnection locomotionEventsConnection;
+    [SerializeField] private Material blackSkybox;
+    [SerializeField] private Material spaceSkybox;
+    [SerializeField] private GameObject livingRoom;
+    [SerializeField] private GameObject spawnPoint;
+    [SerializeField] private GameObject cinematicScreen;
 
     void Start()
     {
+        livingRoom.SetActive(false);
+        RenderSettings.skybox = blackSkybox;
         videoPlayer.loopPointReached += OnVideoEnd;
         videoPlayer.Play();
     }
 
     void OnVideoEnd(VideoPlayer vp)
     {
+        livingRoom.SetActive(true);
+        spawnPoint.SetActive(false);
+        cinematicScreen.SetActive(false);
+
         LocomotionEvent teleportEvent = new LocomotionEvent(
             0,
             new Pose(mainMapSpawnPoint.position, mainMapSpawnPoint.rotation),
@@ -23,5 +34,8 @@ public class PrologueVideoController : MonoBehaviour
             LocomotionEvent.RotationType.Absolute
         );
         locomotionEventsConnection.HandleLocomotionEvent(teleportEvent);
+
+        RenderSettings.skybox = spaceSkybox;
+        DynamicGI.UpdateEnvironment();
     }
 }
