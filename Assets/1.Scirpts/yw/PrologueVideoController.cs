fe23242934913730ee06a69d90a39915e12883d3
LYWW0120 <88764790+LYWW0120@users.noreply.github.com>
2026-03-19 23:13:05 +0900

    teleport update

--- Assets/1.Scirpts/yw/PrologueVideoController.cs
+++ Assets/1.Scirpts/yw/PrologueVideoController.cs
@@ -1,16 +1,27 @@
+using Oculus.Interaction.Locomotion;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PrologueVideoController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private Transform mainMapSpawnPoint;
+    [SerializeField] private LocomotionEventsConnection locomotionEventsConnection;
+
     void Start()
     {
-        
+        videoPlayer.loopPointReached += OnVideoEnd;
+        videoPlayer.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnVideoEnd(VideoPlayer vp)
     {
-        
+        LocomotionEvent teleportEvent = new LocomotionEvent(
+            0,
+            new Pose(mainMapSpawnPoint.position, mainMapSpawnPoint.rotation),
+            LocomotionEvent.TranslationType.Absolute,
+            LocomotionEvent.RotationType.Absolute
+        );
+        locomotionEventsConnection.HandleLocomotionEvent(teleportEvent);
     }
 }
